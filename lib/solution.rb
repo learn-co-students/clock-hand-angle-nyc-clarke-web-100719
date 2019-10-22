2d4ea7011cfbf0ef65ea685bf12a2db37910dbfa
github username <github email address>
2019-10-22 10:11:50 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+    time_array = time.split(":")
+    hour = time_array[0].to_i
+    minute = time_array[1].to_i
+    lowest_difference_between_time(hour, minute)
 end
+
+def lowest_difference_between_time(hour, minute)
+    diff = hour_angle_from_zero(hour, minute) - minute_angle_from_zero(minute)
+    diff = diff.abs
+    diff = 360 - diff if diff > 180
+    diff
+end
+
+def hour_angle_from_zero(hour, minute)
+    hour * 30 + minute * 0.5
+end
+
+def minute_angle_from_zero(minute)
+    minute * 6
+end
+
+
+clock_angle("2:00")
