b200d6270d1130673de600ccec53714aa3c94e8b
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-22 09:53:34 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,9 @@
 def clock_angle(time)
-    # code your solution here
+    arr = time.split(":")
+    
+    if (arr[0].to_i * 30 + arr[1].to_i * 0.5 - arr[1].to_i * 6) <= 180
+        arr[0].to_i * 30 + arr[1].to_i * 0.5 - arr[1].to_i * 6
+    else
+        360 - (arr[0].to_i * 30 + arr[1].to_i * 0.5 - arr[1].to_i * 6)
+    end
 end
